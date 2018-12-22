fa03452a103fba7f762781a551e1d05842adbf00
reekol <cupuyc@gmail.com>
2018-12-22 22:56:10 +0200

    Update create.crt.sh
    added verbose to  cp

--- crt/create.crt.sh
+++ crt/create.crt.sh
@@ -2,6 +2,6 @@
 
 openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem -subj "/C=BG/ST=Sofia/L=Sofia/O=SeQR.link/OU=SeQR/CN=*.seqr.link"
 a2enmod ssl rewrite
-cp ./seqr.link.conf /etc/apache2/sites-available
+cp -v ./seqr.link.conf /etc/apache2/sites-available
 a2ensite seqr.link
 htpasswd -b -c ../public/.htpasswd admin admin123
